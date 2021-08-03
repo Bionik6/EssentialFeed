9d23ef450146eb038fa3ceb99167a48c0ccdab6e
Ibrahima Ciss <bionik6@gmail.com>
2021-08-03 10:24:34 +0000

    Replace optional type with enum

--- Sources/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ Sources/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -1,8 +1,12 @@
 import Foundation
 
+public enum HTTPClientResult {
+  case success(HTTPURLResponse)
+  case failure(Error)
+}
 
 public protocol HTTPClient {
-  func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+  func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 
@@ -21,9 +25,12 @@ public final class RemoteFeedLoader {
   }
   
   public func load(completion: @escaping (Error) -> ()) {
-    client.get(from: url) { error, response in
-      completion(response != nil ? .invalidData : .connectivity)
+    client.get(from: url) { result in
+      switch result {
+      case .success: completion(.invalidData)
+      case .failure: completion(.connectivity)
+      }
     }
   }
 }
- 
+
